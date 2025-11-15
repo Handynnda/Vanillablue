1edabcbb63c8c8665bba812e5d2887ce3ff844b4
Handy Nanda Fachrizal <149317690+Handynnda@users.noreply.github.com>
2025-11-16 03:11:41 +0700

    update tampilan list harga

--- resources/views/galeri/galeribaby.blade.php
+++ resources/views/galeri/galeribaby.blade.php
@@ -1,5 +1,9 @@
 @include ('header')
 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  
 <style>
 /* === GLOBAL === */
 body { 
--- resources/views/listharga.blade.php
+++ resources/views/listharga.blade.php
@@ -4,64 +4,53 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <link rel="stylesheet" href="assets/css/style.css">
 
+<style>
+    .hidden { 
+      display: none;
+    }
+    .category-nav a.active {
+        font-weight: bold;
+        border-bottom: 2px solid #000;
+    }
+</style>
+
 <main class="pricing-page">
+
   <section class="pricing-header">
     <div class="container">
       <h1>Lis Harga Di VanillaBlue Photostudio</h1>
       <nav class="category-nav">
-        <a href="#">Baby & Kids</a>
-        <a href="#">Birthday</a>
-        <a href="#">Maternity</a>
-        <a href="#">Prewed</a>
-        <a href="#" class="active">Graduation</a>
-        <a href="#">Family</a>
-        <a href="#">Group</a>
-        <a href="#">Couple</a>
-        <a href="#">Personal</a>
-        <a href="#">Pas Foto</a>
-        <a href="#">Print & Frame</a>
+        <a href="#" data-target="baby">Baby & Kids</a>
+        <a href="#" data-target="birthday">Birthday</a>
+        <a href="#" data-target="maternity">Maternity</a>
+        <a href="#" data-target="prewed">Prewed</a>
+        <a href="#" data-target="graduation" class="active">Graduation</a>
+        <a href="#" data-target="family">Family</a>
+        <a href="#" data-target="group">Group</a>
+        <a href="#" data-target="couple">Couple</a>
+        <a href="#" data-target="personal">Personal</a>
+        <a href="#" data-target="pasfoto">Pas Foto</a>
+        <a href="#" data-target="printf" >Print & Frame</a>
+        {{-- <div>
+          <a href="baby">Baby & Kids</a>
+          <a href="birthday">Birthday</a>
+          <a href="#">Maternity</a>
+          <a href="prewed">Prewed</a>
+          <a href="graduation" class="active">Graduation</a>
+          <a href="family">Family</a>
+          <a href="#">Group</a>
+          <a href="couple">Couple</a>
+          <a href="#">Personal</a>
+          <a href="#">Pas Foto</a>
+          <a href="#">Print & Frame</a>
+        </div> --}}
       </nav>
     </div>
   </section>
 
-
-{{-- card untuk photo --}}
-  {{-- <section class="pricing-banner" style="text-align:center; margin-bottom:40px;">
-      <div class="banner-grid" 
-          style="display:grid; grid-template-columns: repeat(3, 1fr); gap:10px; max-width: 800px; margin: 0 auto; justify-items:center;">
-          <div class="card">
-              <img src="assets/images/birthday.jpg" alt="birthday" 
-                  style="max-width:100%; height:auto; border-radius:15px;">
-          </div>
-          <div class="card">
-              <img src="assets/images/birthday.jpg" alt="birthday" 
-                  style="max-width:100%; height:auto; border-radius:15px;">
-          </div>
-          <div class="card">
-              <img src="assets/images/birthday.jpg" alt="birthday" 
-                  style="max-width:100%; height:auto; border-radius:15px;">
-          </div>
-          <div class="card">
-              <img src="assets/images/birthday.jpg" alt="birthday" 
-                  style="max-width:100%; height:auto; border-radius:15px;">
-          </div>
-          <div class="card">
-              <img src="assets/images/birthday.jpg" alt="birthday" 
-                  style="max-width:100%; height:auto; border-radius:15px;">
-          </div>
-          <div class="card">
-              <img src="assets/images/birthday.jpg" alt="birthday" 
-                  style="max-width:100%; height:auto; border-radius:15px;">
-          </div>
-      </div>
-
-      <p style="margin-top:15px; font-size:1rem; color:#555;">
-          Berikut paket Graduation dari VanillaBlue Photostudio
-      </p>
-  </section> --}}
-
-
-  <section class="pricing-cards">
+            {{-- section page --}}
+          {{-- graduation section --}}
+  <section class="pricing-cards hidden" id="graduation">
     <div class="container">
       <div class="slider-wrapper">
         <button class="slider-btn prev">&#10094;</button>
@@ -72,6 +61,8 @@
               <li>15 menit, All file google drive</li>
               <li>10 edit</li>
               <li>max 4 orang</li>
+              <li>&nbsp;</li>
+              <li>&nbsp;</li>
               <li>&nbsp;</li>
             </ul>
             <p class="price">Rp 250.000</p>
@@ -84,6 +75,8 @@
               <li>10 edit</li>
               <li>max 10 orang</li>
               <li>cetak 10R p2</li>
+              <li>&nbsp;</li>
+              <li>&nbsp;</li>
             </ul>
             <p class="price">Rp 350.000</p>
             <a href="booking" class="btn-booking" style="text-decoration:none; color:#000;">BOOKING</a>
@@ -95,6 +88,34 @@
               <li>10 edit</li>
               <li>max 10 orang</li>
               <li>Cetak 12R + Frame</li>
+              <li>&nbsp;</li>
+              <li>&nbsp;</li>
+            </ul>
+            <p class="price">Rp 550.000</p>
+            <a href="booking" class="btn-booking" style="text-decoration:none; color:#000;">BOOKING</a>
+          </div>          
+          <div class="card">
+            <h3>GRADUATION D</h3>
+            <ul>
+              <li>15 menit, All file google drive</li>
+              <li>10 edit</li>
+              <li>max 10 orang</li>
+              <li>Cetak 12R + Frame</li>
+              <li>&nbsp;</li>
+              <li>&nbsp;</li>
+            </ul>
+            <p class="price">Rp 550.000</p>
+            <a href="booking" class="btn-booking" style="text-decoration:none; color:#000;">BOOKING</a>
+          </div>
+          <div class="card">
+            <h3>GRADUATION E</h3>
+            <ul>
+              <li>15 menit, All file google drive</li>
+              <li>10 edit</li>
+              <li>max 10 orang</li>
+              <li>Cetak 12R + Frame</li>
+              <li>&nbsp;</li>
+              <li>&nbsp;</li>
             </ul>
             <p class="price">Rp 550.000</p>
             <a href="booking" class="btn-booking" style="text-decoration:none; color:#000;">BOOKING</a>
@@ -104,7 +125,152 @@
       </div>
     </div>
   </section>
-
+            {{-- baby section --}}
+  <section class="pricing-cards hidden" id="baby">
+  <div class="container">
+    <div class="slider-wrapper">
+      <button class="slider-btn prev">&#10094;</button>
+      <div class="cards-container">
+        <div class="card">
+          <h3>BABY & KIDS</h3>
+          <ul>
+            <li>15 menit</li>
+            <li>All file google drive</li>
+            <li>10 edit</li>
+            <li>&nbsp;</li>
+            <li>&nbsp;</li>
+            <li>&nbsp;</li>
+          </ul>
+          <p class="price">IDR 200.000</p>
+          <a href="booking" class="btn-booking" style="text-decoration: none; color: #000;">BOOKING</a>
+        </div>
+      </div>
+      <button class="slider-btn next">&#10095;</button>
+    </div>
+  </div>
+  </section>
+            {{-- birthday section --}}
+    <section class="pricing-cards hidden" id="birthday">
+    <div class="container">
+      <div class="slider-wrapper">
+        <button class="slider-btn prev">&#10094;</button>
+        <div class="cards-container">
+          <div class="card">
+              <h3>BIRTHDAY</h3>
+              <ul>
+                <li>20 menit</li>
+                <li>All file google drive</li>
+                <li>&nbsp;</li>
+                <li>&nbsp;</li>
+                <li>&nbsp;</li>
+                <li>&nbsp;</li>
+              </ul>
+              <p class="price">IDR 300.000</p>
+            <a href="booking" class="btn-booking" style="text-decoration: none; color: #000;">BOOKING</a>
+          </div>
+        </div>
+        <button class="slider-btn next">&#10095;</button>
+      </div>
+    </div>
+    </section>
+            {{-- couple section --}}
+    <section class="pricing-cards hidden" id="couple">
+    <div class="container">
+      <div class="slider-wrapper">
+        <button class="slider-btn prev">&#10094;</button>
+        <div class="cards-container">
+          <div class="card">
+            <h3>COUPLE</h3>
+              <ul>
+                <li>15 menit</li>
+                <li>All file google drive</li>
+                <li>10 edit</li>
+                <li>&nbsp;</li>
+                <li>&nbsp;</li>
+                <li>&nbsp;</li>
+              </ul>
+              <p class="price">IDR 200.000</p>
+            <a href="booking" class="btn-booking" style="text-decoration: none; color: #000;">BOOKING</a>
+          </div>
+        </div>
+        <button class="slider-btn next">&#10095;</button>
+      </div>
+    </div>
+    </section>
+            {{-- family section --}}
+    <section class="pricing-cards hidden" id="family">
+    <div class="container">
+      <div class="slider-wrapper">
+        <button class="slider-btn prev">&#10094;</button>
+        <div class="cards-container">
+          <div class="card">
+            <h3>FAMILY A</h3>
+            <ul>
+              <li>15 menit</li>
+              <li>All file google drive</li>
+              <li>10 edit</li>
+              <li>Berlaku untuk 1 kostum</li>
+              <li>Max 5 orang</li>
+              <li>&nbsp;</li>
+            </ul>
+            <p class="price">IDR 250.000</p>
+            <a href="booking" class="btn-booking" style="text-decoration: none; color: #000;">BOOKING</a>
+          </div>
+          <div class="card">
+            <h3>FAMILY B</h3>
+            <ul>
+              <li>15 menit</li>
+              <li>All file google drive</li>
+              <li>10 edit</li>
+              <li>Berlaku untuk 1 kostum</li>
+              <li>Max 10 orang</li>
+              <li>Cetak 10rp + frame 1</li>
+            </ul>
+            <p class="price">IDR 400.000</p>
+            <a href="booking" class="btn-booking" style="text-decoration: none; color: #000;">BOOKING</a>
+          </div>
+          <div class="card">
+            <h3>FAMILY C</h3>
+            <ul>
+              <li>15 menit</li>
+              <li>All file google drive</li>
+              <li>10 edit</li>
+              <li>Berlaku untuk 1 kostum</li>
+              <li>Max 10 orang</li>
+              <li>Cetak 16rp + frame 1</li>
+            </ul>
+            <p class="price">IDR 600.000</p>
+            <a href="booking" class="btn-booking" style="text-decoration: none; color: #000;">BOOKING</a>
+          </div>
+        </div>
+        <button class="slider-btn next">&#10095;</button>
+      </div>
+    </div>
+    </section>
+            {{-- prewed section --}}
+    <section class="pricing-cards hidden" id="prewed">
+    <div class="container">
+      <div class="slider-wrapper">
+        <button class="slider-btn prev">&#10094;</button>
+        <div class="cards-container">
+          <div class="card">
+            <h3>PRE-WEDDING</h3>
+            <ul>
+              <li>30 menit</li>
+              <li>All file google drive</li>
+              <li>10 edit</li>
+              <li>Berlaku untuk 1 kostum</li>
+              <li>&nbsp;</li>
+              <li>&nbsp;</li>
+            </ul>
+            <p class="price">IDR 500.000</p>
+            <a href="booking" class="btn-booking" style="text-decoration: none; color: #000;">BOOKING</a>
+          </div>
+        </div>
+        <button class="slider-btn next">&#10095;</button>
+      </div>
+    </div>
+    </section>
   <section class="social-updates">
     <div class="container">
       <div class="social-card" 
@@ -130,17 +296,42 @@
 
 <!-- === SLIDER SCRIPT === -->
 <script>
-  const container = document.querySelector('.cards-container');
-  const prev = document.querySelector('.slider-btn.prev');
-  const next = document.querySelector('.slider-btn.next');
+// === SLIDER SCRIPT ===
+const container = document.querySelector('.cards-container');
+const prev = document.querySelector('.slider-btn.prev');
+const next = document.querySelector('.slider-btn.next');
 
-  const scrollStep = 300;
+const scrollStep = 300;
 
-  next.addEventListener('click', () => {
-    container.scrollBy({ left: scrollStep, behavior: 'smooth' });
+next?.addEventListener('click', () => {
+  container.scrollBy({ left: scrollStep, behavior: 'smooth' });
+});
+
+prev?.addEventListener('click', () => {
+  container.scrollBy({ left: -scrollStep, behavior: 'smooth' });
+});
+
+document.querySelectorAll('.category-nav a').forEach(menu => {
+  menu.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // SEMBUNYIKAN SEMUA SECTION
+    document.querySelectorAll('.pricing-cards')
+      .forEach(section => section.classList.add('hidden'));
+
+    // TAMPILKAN SECTION SESUAI TARGET
+    const target = this.getAttribute('data-target');
+    const showSection = document.getElementById(target);
+    if (showSection) showSection.classList.remove('hidden');
+
+    // ACTIVE MENU
+    document.querySelectorAll('.category-nav a')
+      .forEach(a => a.classList.remove('active'));
+    this.classList.add('active');
   });
+});
 
-  prev.addEventListener('click', () => {
-    container.scrollBy({ left: -scrollStep, behavior: 'smooth' });
-  });
+// TAMPILKAN DEFAULT (Graduation)
+document.getElementById("graduation").classList.remove("hidden");
+
 </script>
